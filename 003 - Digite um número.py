b73877f967bb44c07ad0fa7b0c090eda13aefb9d
Daniel Heber da Silva Lima Junior <83608888+Daniel-H-1@users.noreply.github.com>
2022-02-16 15:00:15 -0300

    Add files via upload

---
+++ 003 - Digite um número.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+s = n1+n2
+
+#print('A soma entre', n1, 'e', n2, 'é igual a', s)
+#print('A soma dos números é: ', s)
+print('A soma entre {} e {} é igual a: {}'.format(n1, n2, s))
+
+
