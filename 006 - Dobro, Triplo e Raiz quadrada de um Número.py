8726325c85fb84f72d7efd5c8fb2b4a686f02c8a
Daniel Heber da Silva Lima Junior <83608888+Daniel-H-1@users.noreply.github.com>
2022-02-19 23:44:47 -0300

    Add files via upload

---
+++ 006 - Dobro, Triplo e Raiz quadrada de um Número.py
@@ -0,0 +1,6 @@
+print('Calculando o Dobro, Triplo e a Raiz Quadrada de um Número!!!')
+n = int(input('Digite um número: '))
+
+print('O dobro desse número é: ', n*2)
+print('O triplo desse número é: ', n*3)
+print('A raiz quadrada desse número é: ', n**(1/2))
