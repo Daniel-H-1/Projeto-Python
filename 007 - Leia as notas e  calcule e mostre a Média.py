8a0be704daf7e8aa0ec73fd880f0e1445b8f001a
Daniel Heber da Silva Lima Junior <83608888+Daniel-H-1@users.noreply.github.com>
2022-02-21 00:02:49 -0300

    Add files via upload

---
+++ 007 - Leia as notas e  calcule e mostre a Média.py
@@ -0,0 +1,9 @@
+print('Calculando a nota do aluno!!!')
+
+nome = str(input('Olá, digite seu nome, por gentileza: '))
+n1 = int(input('Digite sua Primeira Nota: '))
+n2 = int(input('Digite a Segunda nota: '))
+m = (n1 + n2) / 2
+
+print('{} sua Média é: {}'.format(nome, m))
+
