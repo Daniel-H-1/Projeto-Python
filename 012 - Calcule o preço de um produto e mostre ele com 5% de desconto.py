efcb5dc7d0ba004e0926f5ba0e56e2238ad545e3
Daniel Heber da Silva Lima Junior <83608888+Daniel-H-1@users.noreply.github.com>
2022-02-26 22:43:42 -0300

    Add files via upload

---
+++ 012 - Calcule o preço de um produto e mostre ele com 5% de desconto.py
@@ -0,0 +1,7 @@
+print('012 - Calcule o preço de um produto e mostre ele com 5% de desconto')
+print('Dica coloque  o preço so seu produto neste formato -> 559.69 \n')
+
+p = float(input('Digite o seu salario: '))
+d = p * 0.05
+r = p - d
+print('O seu produto com 5% de desconto é igual á: {}'.format(r))
