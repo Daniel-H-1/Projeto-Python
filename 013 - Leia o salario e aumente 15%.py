b7896f95a07a5b160626bd9c012668e8ddb0192d
Daniel Heber da Silva Lima Junior <83608888+Daniel-H-1@users.noreply.github.com>
2022-03-03 10:53:08 -0300

    Add files via upload

---
+++ 013 - Leia o salario e aumente 15%.py
@@ -0,0 +1,8 @@
+print('O Salario com aumento de 15%  !!!')
+print('Dica coloque seu salario neste formato -> 3559.69 \n')
+
+s = float(input('Digite o seu salario: '))
+a = s * 0.15
+sf = s + a
+
+print('O seu salario com 15% de aumento é igual á {} Reais'.format(sf))
