0d3e172ee7dc380605e4d6ee0dbe40b460f7cf5b
Daniel Heber da Silva Lima Junior <83608888+Daniel-H-1@users.noreply.github.com>
2022-03-04 20:04:11 -0300

    Add files via upload

---
+++ 014 - Conversão de temperaturas.py
@@ -0,0 +1,7 @@
+print('A conversão da temperatura!!!')
+print(' ')
+
+Celsius = float(input('Digite a temperatura em Graus Celsius: '))
+Fahrenheit = (Celsius * 9/5) + 32
+
+print('{} Graus Celsius convertido é igual a {} Fahrenheit'.format(Celsius, Fahrenheit))
