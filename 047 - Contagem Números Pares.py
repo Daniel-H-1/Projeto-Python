2be0a23ed76a5d71b4f576abac3426a5d5ed7c31
Daniel Heber da Silva Lima Junior <83608888+Daniel-H-1@users.noreply.github.com>
2022-04-29 13:19:57 -0300

    Add files via upload

---
+++ 047 - Contagem Números Pares.py
@@ -0,0 +1,9 @@
+print('>>>>> INSTRUÇÕES DO PROGRAMA <<<<<')
+print('''Crie um programa que mostre na tela todos os números pares
+que estão no intervalo entre 1 e 50''')
+
+print(' ')
+print('>>>>> RESOLUÇÃO DO PROGRAMA <<<<<')
+for c in range(0, 51, 2):
+    print(c)
+print('>>>>> FIM <<<<<')
