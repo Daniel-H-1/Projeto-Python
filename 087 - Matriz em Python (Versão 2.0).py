b0b9284ac2a2d2a1ab7e3f202ec5f22b62df04fc
Daniel Heber da Silva Lima Junior <83608888+Daniel-H-1@users.noreply.github.com>
2022-06-29 19:25:57 -0300

    Add files via upload

---
+++ 087 - Matriz em Python (Versão 2.0).py
@@ -0,0 +1,42 @@
+print(f'{"INSTRUÇÕES DO PROGRAMA":=^40}')
+print('''Aprimore o desafio anterior, mostrando no final: 
+
+A) A soma de todas os valores pares digitados.
+B) A soma dos valores da terceira coluna.
+C) O maior valor da segunda linha.''')
+
+print(' ')
+print(f'{"RESOLUÇÃO DO PROGRAMA":=^40}')
+print('MATRIZ EM PYTHON')
+print('-' * 40)
+Matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+somaPares = somaColuna3 = 0
+#recebendo os dados na Lista.
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        Matriz[linha][coluna] = int(input(f'Digite o valor para [{linha}, {coluna}]: '))
+#Escrevendo os dados na tela (PRINT)
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        print(f'[{Matriz[linha][coluna]:^5}]', end="")
+    print()
+#Analisando os valores e somando.
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        #Somando os Pares
+        if Matriz[linha][coluna] % 2 == 0:
+            somaPares += Matriz[linha][coluna]
+        #Somando os números da coluna 3
+        if coluna == 2:
+            somaColuna3 += Matriz[linha][coluna]
+        #Achando o maior número da Linha 2
+        if linha == 1:
+            if coluna == 0:
+                maior = Matriz[linha][coluna]
+            elif Matriz[linha][coluna] > maior:
+                maior = Matriz[linha][coluna]
+print('-' * 40)
+print(f'Soma dos Pares é {somaPares}')
+print(f'A soma da terceira coluna é {somaColuna3}')
+print(f'O maior valor da segunda linha é {maior}')
+print(f'{"FIM":=^40}')
